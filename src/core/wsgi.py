6e4fbdb2cf6bdaed06135b2927beb9b9bb9620ae
oncomouse <oncomouse@gmail.com>
2017-08-04 16:57:42 -0500

    Easier to configure version of wsgi.py.

--- src/core/wsgi.py
+++ src/core/wsgi.py
@@ -10,18 +10,21 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
-
 import os
 import sys
+import site
 
-from django.core.wsgi import get_wsgi_application
+janeway_path = '/path/to/installation'
+virtualenv_path = '/path/to/your/virtualenv/name'
+virtualenv_python_verison = 3.6
 
-# add the project path into the sys.path
-sys.path.append('/path/to/installation/')
-
-# add the virtualenv site-packages path to the sys.path
-sys.path.append('/path/to/your/virtualenv/name/lib/python3.5/site-packages/')
+# add the project path into the sys.path:
+sys.path.append(os.path.join(janeway_path, 'src'))
+# add the site-packages folder to sys.path:
+sys.path.append(os.path.join(virtualenv_path, 'lib', 'python'+str(virtualenv_python_verison), 'site-packages'))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+
+from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
